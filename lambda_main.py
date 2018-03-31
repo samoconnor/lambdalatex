cfa34fb2de45d0e1762ad0ee36eb0cbed5133f5e
Sam O'Connor <sam@octech.com.au>
2018-03-31 13:12:15 +1100

    run pdflatex twice to ensure population of TCO etc

--- lambda_main.py
+++ lambda_main.py
@@ -13,22 +13,22 @@ def main(event, context):
     z = zipfile.ZipFile(io.BytesIO(base64.b64decode(event["input"])))
     z.extractall(path="/tmp/latex")
 
-    r = subprocess.run(["ls", "-laR", "/tmp/latex"])
-
     os.environ['PATH'] += ":/var/task/texlive/2017/bin/x86_64-linux/"
-    os.environ['HOME'] = '/tmp/'
+    os.environ['HOME'] = "/tmp/latex/"
+    os.chdir("/tmp/latex/")
 
     # Run pdflatex...
-    r = subprocess.run(["/var/task/texlive/2017/bin/x86_64-linux/pdflatex",
-                        "-interaction=batchmode",
-                        "-output-directory=/tmp/latex",
-                        "/tmp/latex/document.tex"],
-                       stdout=subprocess.PIPE,
-                       stderr=subprocess.STDOUT)
-    print(r.stdout.decode('utf_8'))
+    for i in [1, 2]:
+        r = subprocess.run(["/var/task/texlive/2017/bin/x86_64-linux/pdflatex",
+                            "-interaction=batchmode",
+                            "-output-directory=/tmp/latex",
+                            "document.tex"],
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT)
+        print(r.stdout.decode('utf_8'))
 
     # Read "document.pdf"...
-    with open('/tmp/latex/document.pdf', 'rb') as f:
+    with open("document.pdf", "rb") as f:
         pdf = f.read()
 
     # Return base64 encoded pdf and stdout log from pdflaxex...
